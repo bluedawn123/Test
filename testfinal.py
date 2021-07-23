acbafd4c7b36c544a034a0b1f8108c1047711341
haemilyjh <haemilyjh@naver.com>
2021-07-23 18:56:21 +0900

    2021-07-23 DL study

---
+++ testfinal.py
@@ -0,0 +1,18 @@
+import tensorflow
+from tensorflow import keras
+import pandas
+import sklearn
+import scipy
+import numpy
+import matplotlib
+#import pydotplus
+#import pydot
+import h5py
+print('tensorflow ' + tensorflow.__version__)
+print('keras ' + keras.__version__)
+print('pandas ' + pandas.__version__)
+print('sklearn ' + sklearn.__version__)
+print('scipy ' + scipy.__version__)
+print('numpy ' + numpy.__version__)
+print('matplotlib ' + matplotlib.__version__)
+print('h5py ' + h5py.__version__)
