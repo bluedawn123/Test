83d3bdfeb4a92cbf10f9d22b60492c4998a0c519
haemilyjh <haemilyjh@naver.com>
2021-11-05 18:53:29 +0900

    코딩테스트 준비

---
+++ listtest.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = np.arange(10)
+x = a.reshape(2, 5)
+print(a)
+print(x)
+
+b = [5, 2, 3, 1]
+print(b[::-1])
+
+arr = np.arange(10)
+print(arr)
+
+print(arr[::2])
+print(arr[1::2])
+print(arr[1:7:1])
