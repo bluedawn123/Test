3a5672ad97db6bc42e92013e10aa245ad7898be9
haemilyjh <haemilyjh@naver.com>
2022-03-22 16:59:52 +0900

    vue로 blog 만들기

---
+++ vue2/blog/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-4" >
+  <h5>윤준호 개발자의 블로그 입니다.</h5>
+  <p> Vue로 만들었슴 </p>
+</div>
+
+<TheList/>
+
+</template>
+
+
+
+<script>
+import TheList from './components/TheList.vue'
+import blog from './assets/blog.js'
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      블로그글 : blog,
+    }
+    
+  },
+  components: {
+    TheList : TheList,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ vue2/blog/src/components/TheList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>블ddd목</h5>
+    <p>블로그 글 날짜</p>
+  </div>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜</p>
+  </div>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'TheList',
+    props : {   
+        블로그글 : Array,
+    },
+
+}
+</script>
+<style>
+
+</style>
