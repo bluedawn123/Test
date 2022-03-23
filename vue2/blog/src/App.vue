5f4631e1be1570838ec52abb63bae689e9005b19
haemilyjh <haemilyjh@naver.com>
2022-03-23 16:50:39 +0900

    vue로 blog만들기 공부

--- vue2/blog/src/App.vue
+++ vue2/blog/src/App.vue
@@ -8,10 +8,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <router-link to = "/"><a class="nav-link active" aria-current="page" href="#">Home</a></router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <router-link to = "/list"><a class="nav-link" href="#">List</a></router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to = "/test"><a class="nav-link" href="#">Test</a></router-link>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -36,19 +39,48 @@
   </div>
 </nav>
 
-<div class="container mt-4" >
-  <h5>윤준호 개발자의 블로그 입니다.</h5>
-  <p> Vue로 만들었슴 </p>
-</div>
 
-<TheList/>
+<!-- <router-link to="/list">리스트페이지 가기</router-link> -->
+
+
+<!------------------------------------------------------ 라우터 -------------------------------------------------->
+<!-- /list로 접속하면 이게 <TheList>가 되는 거임. 그래서 아래 주석처리함 -->
+<router-view :블로그글="블로그글"></router-view> 
+<!-- <TheList :블로그글="블로그글"/> 라우터 써니 그냥 지움-->
+<!------------------------------------------------------라우터 끝 -------------------------------------------------->
+
+
 
 </template>
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <script>
-import TheList from './components/TheList.vue'
+// import TheList from './components/TheList.vue'
 import blog from './assets/blog.js'
 
 
@@ -61,7 +93,7 @@ export default {
     
   },
   components: {
-    TheList : TheList,
+    // TheList : TheList,
 
   }
 }
