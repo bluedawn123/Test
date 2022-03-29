d13a33951c64f5cfdf0f0b283a14573ad42caf29
haemilyjh <haemilyjh@naver.com>
2022-03-29 21:11:03 +0900

    vuestagram 공부

--- vuestagram/vuestagram/src/App.vue
+++ vuestagram/vuestagram/src/App.vue
@@ -165,4 +165,16 @@ ul {
   border-right: 1px solid #eee;
   border-left: 1px solid #eee;
 }
+
+#app2 {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
 </style>
