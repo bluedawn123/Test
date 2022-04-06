732b5d0b47c8006e3fbeec9e19b430748226befd
haemilyjh <haemilyjh@naver.com>
2022-04-06 21:04:29 +0900

    vuex store 사용하기

--- vuestagram/vuestagram/src/main.js
+++ vuestagram/vuestagram/src/main.js
@@ -7,5 +7,5 @@ let emitter = mitt();
 let app = createApp(App);
 app.config.globalProperties.emitter = emitter; // app.config.globalProperties 이 부분이 모든 컴포넌트에서 사용할 수 있는 변수같은걸 등록할 수 있게 도와주는 일종의 object 자료
 
-app.mount('#app') 
+// app.mount('#app') 
 app.use(store).mount('#app')
