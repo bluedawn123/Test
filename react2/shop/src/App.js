439ddf16300e9947fdef85b5a5a3b06feab645dc
haemilyjh <haemilyjh@naver.com>
2022-04-25 21:10:39 +0900

    리액트로 간단한 쇼핑몰 만들어보기

--- react2/shop/src/App.js
+++ react2/shop/src/App.js
@@ -35,9 +35,9 @@ function App() {
             <Nav.Link as={Link} to="/detail"> Service </Nav.Link> 
 
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.1">1</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">2</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">3</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
