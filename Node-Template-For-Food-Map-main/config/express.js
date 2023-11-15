5e3378939bd125943cf02bb401a8cc55d129973a
haemilyjh <haemilyjh@naver.com>
2023-11-15 21:36:00 +0900

    맛집 지도 노드js 페키지

---
+++ Node-Template-For-Food-Map-main/config/express.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const compression = require("compression");
+const methodOverride = require("method-override");
+var cors = require("cors");
+
+module.exports = function () {
+  const app = express();
+
+  app.use(compression());
+
+  app.use(express.json());
+
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(methodOverride());
+
+  app.use(cors());
+  app.use(express.static("/home/ubuntu/food-map/front"));
+
+  // app.use(express.static(process.cwd() + '/public'));
+
+  //직접구현해야하는 모듈
+  require("../src/routes/indexRoute")(app);
+
+  return app;
+};
