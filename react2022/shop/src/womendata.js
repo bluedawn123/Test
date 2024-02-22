fef073b2708b378f91c3cff1c22a3c6f943c801f
haemilyjh <haemilyjh@naver.com>
2024-02-22 23:50:23 +0900

    2022 신발 샵

---
+++ react2022/shop/src/womendata.js
@@ -0,0 +1,47 @@
+let womendata = [
+    {
+      id : 0,
+      title : "Rainbow Running",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Purple Sneakers",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Driving shoes",
+      content : "Born in the States",
+      price : 130000
+    },
+
+    {
+        id : 3,
+        title : "Sky",
+        content : "Born in the States",
+        price : 130000
+    },
+
+    {
+        id : 4,
+        title : "Red JJori",
+        content : "Born in the States",
+        price : 130000
+    },
+
+    {
+        id : 5,
+        title : "DarkGray Sneakers",
+        content : "Born in the States",
+        price : 130000
+    },
+
+    
+]
+
+export default womendata
